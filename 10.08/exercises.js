701cfbf11c57e1233e622e42a796e4a6f8566591
Andrijana Mladenovic <andrijanaanci@gmail.comm>
2022-10-20 20:53:24 +0200

    This is a new commit for what I originally planned to be amended

---
+++ 10.08/exercises.js
@@ -0,0 +1,110 @@
+'use strict'
+var h =3;
+var j = -7;
+var k = 2;
+
+
+
+
+
+
+var a = -5;
+var b = -2;
+var c = -6;
+var d = 0 ;
+var f = -1;
+
+if (a > b && a > c && a > d && a > f)
+{
+    console.log(a);
+}
+else if (b > a && b > c && b > d && b > f)
+{
+    console.log(b);
+}
+else if (c > a && c > b && c > d && c > f)
+{
+    console.log(c);
+}
+else if (d > a && d > c && d > b && d > f)
+{
+    console.log(d);
+}
+else
+{
+    console.log(f);
+}
+
+var x= 0;
+var y= -1;
+var z= 4;
+
+if (x > y && x > z){
+if ( y > z){
+  console.log(x + "," + y + " , " + z);
+}else {
+  console.log(x + "," + z + "," +y); 
+}}
+
+else if (y > x && y > z){
+if(x > z){
+  console.log( y + "," + x + "," + z);
+  }else {
+    console.log ( y + "," + z + "," + x);
+  }}
+
+  else if(z > x && z >y){
+    if (x > y){
+      console.log(z + " , " + x + " , " + y)
+    }else {
+      console.log (z + "," + y + " , " + x)
+    }
+  }
+
+var i = 10 ;
+var o = 7 ; 
+
+if (i % 2==0 && typeof Number)
+{
+  console.log(i +  'is number and divisible by 2')
+}
+ else {
+  console.log('x')
+}
+
+if (o % 2==0 && typeof Number)
+{
+  console.log(o +  'is number and divisible by 2')
+}
+  else {
+  console.log('x')
+}
+
+var t = 10;
+var u = 25;
+if(t > u){
+  console.log ( t + ' is grattest than' + u)
+}
+else{
+  console.log(u + ' is grattest than' + t)
+}
+
+var celsius = 60;
+var fahrenheit;
+fahrenheit = (9 * celsius / 5) + 32;
+
+console.log (celsius + ' C ' + ' is ' + fahrenheit + ' F ')
+
+var l = 13;
+var u = 11;
+var w = u - l
+if(u > l){
+  console.log(u - l + w)
+}else {
+  console.log(l - u)
+}
+
+
+var m = 12.5;
+var n = 8.8;
+
---
+++ 10.08/script.js
@@ -0,0 +1,102 @@
+'use strict'
+
+
+var A;
+
+var good;
+var B;
+var raspolozenje;
+raspolozenje = 'good'; 
+console.log(raspolozenje);
+
+
+var c;
+c = 3 - 2 - 31
+console.log(c);
+
+
+var d;
+d = 3 * 5 - 7 / 3;
+console.log(d);
+
+var u;
+u = 4 / 2 - 1.14 * 3;
+console.log(u);
+
+
+var kg;
+kg = 1000;
+var bananas;
+bananas = 1.2 * kg;
+console.log(bananas);
+
+
+var ann;
+var secund;
+var tom;
+secund = 60;
+ann = 5;
+tom = 30;
+console.log(ann * secund);
+console.log(tom * secund);
+
+var mb;
+mb = 1024;
+var memory;
+memory = 4 * mb;
+console.log(memory);
+
+var kb;
+var gb;
+gb = 1000000
+kb = 98;
+console.log(gb / kb);
+
+var name;
+name = 'Andrijana';
+console.log(typeof name);
+
+var age;
+age = 27;
+console.log (typeof age);
+
+var cat;
+cat = true;
+console.log(typeof cat);
+
+
+console.log(typeof "number")
+console.log(typeof true)
+console.log(typeof null)
+console.log(typeof "false")
+console.log(typeof 56)
+
+
+var p;
+p = true;
+
+var q;
+q = true;
+
+console.log(p && q);
+console.log(p || q);
+
+console.log(age < 0) ;
+console.log(age > 120);
+
+
+var speed;
+speed = 50;
+
+var safe;
+safe = speed > 60 && speed < 120;
+
+
+console.log(safe)
+
+
+
+
+
+
+
