2269d30e4b2267b9edbf6cc03c858e42f375c25d
AndrijanaMladenovic <101589265+AndrijanaMladenovic@users.noreply.github.com>
2022-10-22 08:52:14 +0200

    Add files via upload

---
+++ arrays.js
@@ -0,0 +1,25 @@
+//Prvi zadatak
+var mounths = ['januar' ,'februar','mart','april','maj','jun','jul','avgust','septembar','oktobar','novembar','decembar',];
+console.log(mounths[5],mounths[9],mounths[0]);
+
+//Drugi zadatak
+var week = ['monday' , 'thuesday' , 'wednesday' ,'thursday' ,'friday' ,'saturday' ,'sunday'];
+console.log(week[6]);
+
+//Treci zadatak
+var a = [2, -4, 5, -2, -11]
+console.log(a[1],a[3],a[4]);
+
+//Cetvrti zadatak 
+var b = [5, 15, -5, 20, 12, 18, 72, 14, 9];
+console.log(b[1],b[4],b[5],b[6],b[8]);
+
+//Peti zadatak
+var c = [[1,2,1,24],[8,11,9,4],[7,0,7,27],[7,4,28,14],[3,10,26,7]];
+console.log(c[0][3]);
+
+//Peti zadatak pod a
+console.log(c[0][2]);
+
+//Peti zadatak pod b 
+console.log(c[2][1]);
