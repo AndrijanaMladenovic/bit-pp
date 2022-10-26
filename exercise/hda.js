1e8d9abff2c620a11a9545547cbac95b867da664
Andrijana Mladenovic <andrijanaanci@gmail.comm>
2022-10-27 01:42:29 +0200

    da

---
+++ exercise/hda.js
@@ -0,0 +1,19 @@
+
+var a = 5;
+var cube = a * a;
+
+console.log(`${a}up to cube equals ${cube}`);
+
+var c = [1,2,3,4,5];
+var factorial = [0] * [1]  * [2] * [3] * [4] * [5];
+console.log(`The factorial of ${c} is ${factorial}`);
+
+
+var n,sum = 0;
+for(var n = 2 ; n <= 10 ; n+=2){
+  sum = n + n;
+  console.log(`The even numbers are ${n}`)
+  
+}
+console.log(`sum is ${sum+n-n}`)
+
